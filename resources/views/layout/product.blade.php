9cafdcff6ec8b643f83d0025fcde99c5f0908bf6
Pedrolins157 <sgtlins02@gmail.com]>
2024-05-11 11:21:56 -0300

    solucionado o problema de carregamento das fotos

--- resources/views/layout/product.blade.php
+++ resources/views/layout/product.blade.php
@@ -3,7 +3,11 @@
         <div class="col-12  d-flex justify-content-center">
             <img src="img/sistema.jpg" alt=""  width="350px" style="border-radius:15px 15px 0 0;box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;">
         </div> 
-        <div class="col-12" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+        <div class="row col-12 justify-content-evenly" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+            <div class="col-1 text-center text-light block-option" style="background-color: #21D4FD;background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);">Erp</div>
+            <div class="col-4 text-center text-light block-option" style="background-color: #21D4FD;background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);">Gerenciamento</div>
+            <div class="col-3 text-center text-light block-option" style="background-color: #08AEEA;background-image: linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);">Vendas(Pdv)</div>
+            <div class="col-4 text-center text-light block-option" style="background-color: #FFE53B;background-image: linear-gradient(147deg, #FFE53B 0%, #FF2525 74%);">Delivery food</div>    
             <p class="fs-2"><b>Sistemas de gerenciamento</b></p> 
             <p  style="max-width:350px;">Automatize processos e aumente a produtividade com nossos sistemas de gerenciamento.</p>
         </div>
@@ -13,7 +17,10 @@
         <div class="col-12 d-flex justify-content-center">
             <img src="img/site.jpg" alt=""  width="350px" style="border-radius:15px 15px 0 0;box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;">
         </div> 
-        <div class="col-12 product-card" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+        <div class="row col-12 product-card justify-content-evenly" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+            <div class="col-3 text-center text-light block-option" style="background-color: #4158D0;background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);">Interativos</div>
+            <div class="col-3 text-center text-light block-option" style="background-color: #FBAB7E;background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);">Empresarial</div>
+            <div class="col-5 text-center text-light block-option" style="background-color: #8BC6EC;background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);">Pequenos negocios</div>
             <p class="fs-2"><b>Sites profissionais</b></p> 
             <p  style="max-width:350px;">Desenvolvimento de sites profissionais que destacam a sua marca.</p>
         </div>
@@ -22,7 +29,10 @@
         <div class="col-12 d-flex justify-content-center">
             <img src="img/portfolio.jpg" alt=""  width="350px" style="border-radius:15px 15px 0 0;box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;">
         </div> 
-        <div class="col-12 product-card" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+        <div class="row col-12 product-card justify-content-evenly" style="background-color:#ffffff;width:350px;border-radius:0 0 10px 10px;padding:20px;">
+            <div class="col-3 text-center text-light block-option" style="background-color: #4158D0;background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);">Interativos</div>
+            <div class="col-3 text-center text-light block-option" style="background-color: #FBAB7E;background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);">Empresarial</div>
+            <div class="col-5 text-center text-light block-option" style="background-color: #8BC6EC;background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);">Pequenos negocios</div>    
             <p class="fs-2"><b>Portfólio</b></p> 
             <p  style="max-width:350px;">De sites elegantes a sistemas eficientes, nosso portfólio reflete nosso compromisso com a excelência e a inovação.</p>
         </div>
