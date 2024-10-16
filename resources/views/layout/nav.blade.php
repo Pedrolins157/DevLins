028536e88b7a4f8f1fede02838f42542f962b792
Pedro Henrique <sgtlins02@gmail.com>
2024-10-16 00:42:31 -0300

    continuidade

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,8 +1,8 @@
-<nav class="row navbar navbar-expand-lg text-light" >
+<!-- <nav class="row navbar navbar-expand-lg text-light" >
   <div class="col-3  container-fluid text-light" style="margin-left:0;">
     <img src="img/logoo.png" alt="" style="margin-top:15px;">
   </div>
-    <div class="col-5 response-nav">
+    <div class=" col-6 response-nav">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item ms-4">
           <a class="nav-link text-light hvr-grow" aria-current="page" href="#">Produtos</a>
@@ -15,10 +15,9 @@
             Serviços
           </a>
           <ul class="dropdown-menu ">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Consultoria</a></li>
+            <li><a class="dropdown-item" href="#">Orçamento</a></li>
+            <li><a class="dropdown-item" href="#">Fale conosco</a></li>
           </ul>
         </li>
         <li class="nav-item ms-4">
@@ -37,5 +36,42 @@
                 </svg>
                 <span>Login</span>
             </button>
+  </div>
+</nav>  --><nav class="navbar navbar-expand-lg text-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <div class="col-3 container-fluid text-light" style="margin-left:0;">
+        <img src="img/logoo.png" alt="" style="margin-top:15px;">
+      </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
+        <li class="nav-item ms-4">
+          <a class="nav-link text-light hvr-grow" aria-current="page" href="#">Produtos</a>
+        </li>
+        <li class="nav-item ms-4">
+          <a class="nav-link text-light hvr-grow" href="#">Sobre nós</a>
+        </li>
+        <li class="nav-item dropdown ms-4">
+          <a class="nav-link dropdown-toggle text-light hvr-grow" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Serviços
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Consultoria</a></li>
+            <li><a class="dropdown-item" href="#">Orçamento</a></li>
+            <li><a class="dropdown-item" href="#">Fale conosco</a></li>
+          </ul>
+        </li>
+        <li class="nav-item ms-4">
+          <a class="nav-link text-light hvr-grow" href="#">Contato</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link text-light hvr-grow" href="#">Interação</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
