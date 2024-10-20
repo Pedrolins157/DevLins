1cad1f0ba3e810483b156c100f253064e4fa0469
Pedro Henrique <sgtlins02@gmail.com>
2024-10-20 18:12:55 -0300

    dado inicio crud

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Descricao;
+use App\Models\service;
+use Illuminate\Http\Request; 
+
 abstract class Controller
 {
-    
+    public function index(){
+
+    }
+    public function create(Request $request){
+        $data =$request->all();
+        
+        if(service::created($data)){
+            
+        }
+
+
+    }
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function edit($id){
+
+    }
+    public function update($id){
+
+    }
+    public function destroy($id){
+
+    }
+
 }
